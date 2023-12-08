df0542e9b87353f2bc97a231dbb7e1c9c637b78f
rinshahan <rinshahan18@gmail.com>
2023-12-08 11:38:15 +0530

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  mainArr: any[] = []
+
+
+  async ngOnInit() {
+    await fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.products, "siajasd");
+
+        this.mainArr = json.products;
+      })
+    console.log(this.mainArr);
+
+
+
+  }
+
+
 }
